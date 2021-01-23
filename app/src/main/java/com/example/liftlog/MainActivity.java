3f44c6938395dd4621eb7220210d995975d310a8
YotamGolan <golanyotam@gmail.com>
2021-01-23 13:24:05 -0800

    Yotams storage

--- app/src/main/java/com/example/liftlog/MainActivity.java
+++ app/src/main/java/com/example/liftlog/MainActivity.java
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 
 import com.google.firebase.auth.*;
+
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mLogoutBtn = findViewById(R.id.btnLogout);
         fAuth = FirebaseAuth.getInstance();
 
